70e866cc7097d6869c1d337330f772a88631639a
Ilya Golovin <74474615+maIIady@users.noreply.github.com>
2023-07-04 18:02:55 +0000

    chore: remove noise

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,8 @@
 <template>
-  <h1 class="test-reference">{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1 class="test-reference"></h1>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <style>
@@ -33,21 +10,6 @@ const count = ref(0)
   color: blue;
 }
 
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
 </style>
 
 <preview lang="md">
