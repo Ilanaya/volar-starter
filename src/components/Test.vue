a09bb6bcad0070d29bee546a5f6f1dff57da9a00
Ilya Golovin <me@ilscc.ru>
2024-04-03 14:54:08 +0300

    chore: more test data

---
+++ src/components/Test.vue
@@ -0,0 +1,33 @@
+<template>
+    <ButtonComp class="set-default-button" :type="type" :mode="mode" :disabled="disabled" :text="buttonText"
+        @click="emit('setDefault')" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import i18n from '@/stores/i18n.js';
+
+const { pgettext } = i18n();
+
+const buttonText = computed(() => pgettext('The button for applying default settings.', 'Reset by default'));
+
+defineProps({
+    mode: {
+        type: String,
+        default: 'base',
+    },
+    type: {
+        type: String,
+        default: 'text',
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+const emit = defineEmits({
+    setDefault: null,
+});
+</script>
+
+<style lang="scss"></style>
